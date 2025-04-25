4f2cba3ad4903e9a925e07b3e53caab244da80ef
Fadhili Njenga <fadhilinm@gmail.com>
2025-04-25 19:09:03 +0300

    Number 2

--- numbers_1.py
+++ numbers_1.py
@@ -1,2 +1,7 @@
+#created a variable called number which got its input from the user
 number = int(input("Enter a number: "))
- 
+#used % and an if statemtn to find if the remainder of the number that the user provided is equal to  0
+if number % 2 == 0:
+  print("It's an even number")
+else:
+   print("It's an odd number")
