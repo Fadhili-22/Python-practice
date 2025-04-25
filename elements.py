11640535ab83233d7bd54f7e79c55f68f8008385
Fadhili Njenga <fadhilinm@gmail.com>
2025-04-25 18:50:58 +0300

    Number one

---
+++ elements.py
@@ -0,0 +1,6 @@
+#I created an array called x which had 4 numbers
+x = [12, 13,15,16]
+#created a variable called ans which found the sum of elements in the array
+Ans=sum(x)
+#used a print function to display the sum of the elements
+print("Total sum:",Ans)
